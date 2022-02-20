6d8af90cb7f14ddb2d24f8b3441a043e780f8232
slawomirwozniak <creative1990@wp.pl>
2022-02-20 22:19:52 +0100

    The first lesson about creating the variables in Java

---
+++ Variables.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+    type_of_variable name_of_variable; //Sample of variable declaration
+    int sample_number; //As above
+    sample_number = 8; //Variable inicialisation
+    //Types of variables:
+    boolean boolean_variable; //It may contains true or false as value
+    byte byte_variable; //It may contains intigers between -128 to 127 as value
+    short short_variable; //It may contains intigers between -32768 to 32767 as value
+    int int_variable; //It may contains intigers between -2147483648 to 2147483647 as value
+    long long_variable; //It may contains intigers between -9223372036854775808 to 9223372036854775807 as value
+    float float_variable; //It may contains floating numbers between -3.4028234663852886E38 to 3.4028234663852886E38
+    double double_variable; //It may contains floating numbers between -1.7976931348623157E308 to 1.7976931348623157E308
+    char char_variable; //It may contains amount of characters between 0 to 65556;
+
+    /*How to create the names of the variables:
+    It may contain only letters, numbers and mentioned signs: $ or  _
+    Name can not start from numbers
+    It can not contain key words such as: class, int, public etc...
+    Names should be written in accordance with convention of Camel Case such as: myNameOfVariable
+    It's possible to use $ and _ but  it is customary not to do this in the names of variables
+    Use names that best describe the meaning of the variable instead of "a", name it according to what it represents, such as "currentSpeed"
+    It's good when names are short and reflect the meaning well, but don't be afraid of long names, if the situation requires it (meaning > length)*/
+
+
